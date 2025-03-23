ead49ee35b22341ca7771ab30e917fbb331645b9
Antim Pal <134076504+iamAntimPal@users.noreply.github.com>
2025-03-23 22:53:27 +0530

    Update 196. Delete Duplicate Emails.sql

--- LeetCode SQL 50 Solution/196. Delete Duplicate Emails.sql
+++ LeetCode SQL 50 Solution/196. Delete Duplicate Emails.sql
@@ -51,3 +51,8 @@ Explanation: john@example.com is repeated two times. We keep the row with the sm
 
 
 
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+DELETE p2 FROM Person p1 
+JOIN Person p2 
+ON p1.email = p2.email AND p1.id < p2.id;
