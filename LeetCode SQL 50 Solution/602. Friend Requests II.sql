0b2ec0603cf4b71e6546722cd4a8bd424b29f65e
Antim Pal <134076504+iamAntimPal@users.noreply.github.com>
2025-03-23 23:17:22 +0530

    Update 602. Friend Requests II.sql

--- LeetCode SQL 50 Solution/602. Friend Requests II.sql
+++ LeetCode SQL 50 Solution/602. Friend Requests II.sql
@@ -53,3 +53,15 @@ Follow up: In the real world, multiple people could have the same most number of
 
 
 
+# Write your MySQL query statement below
+WITH
+    T AS (
+        SELECT requester_id, accepter_id FROM RequestAccepted
+        UNION ALL
+        SELECT accepter_id, requester_id FROM RequestAccepted
+    )
+SELECT requester_id AS id, COUNT(1) AS num
+FROM T
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 1;
