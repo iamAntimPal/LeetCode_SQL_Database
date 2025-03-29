e051ba74f5dbefc987825ed0dfd08c254b22ce39
Antim Pal <134076504+iamAntimPal@users.noreply.github.com>
2025-03-29 12:29:27 +0530

    Create Rising Temperature.sql

---
+++ LeetCode SQL 50 Solution/197. Rising Temperature/Rising Temperature.sql
@@ -0,0 +1,21 @@
+"""197. Rising Temperature"""
+
+WITH PreviousWeatherData AS
+(
+    SELECT 
+        id,
+        recordDate,
+        temperature, 
+        LAG(temperature, 1) OVER (ORDER BY recordDate) AS PreviousTemperature,
+        LAG(recordDate, 1) OVER (ORDER BY recordDate) AS PreviousRecordDate
+    FROM 
+        Weather
+)
+SELECT 
+    id 
+FROM 
+    PreviousWeatherData
+WHERE 
+    temperature > PreviousTemperature
+AND 
+    recordDate = DATE_ADD(PreviousRecordDate, INTERVAL 1 DAY);
